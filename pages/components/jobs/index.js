f3ae3e9cbb31c94f60f793dc4770d67b5223f876
andhikabrosnan <andhikabrosnan@gmail.com>
2022-08-22 07:38:49 +0700

    fix(jobs): fix minor

--- pages/components/jobs/index.js
+++ pages/components/jobs/index.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Heading, Flex, Image, Text, Divider } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/icons";
 import styles from "./styles.module.css";
 
@@ -11,49 +11,59 @@ const Jobs = () => {
 
       {[0, 1, 2, 3].map((item) => {
         return (
-          <Flex justifyContent="center" alignItems="center" p={2}>
-            <Box>
-              <Image
-                borderRadius="full"
-                boxSize="100px"
-                src="https://www.freepnglogos.com/uploads/mcdonalds-png-logo/mcdonalds-png-logo-picture-3.png"
-                alt="Dan Abramov"
-                mr={2}
-              />
-            </Box>
-            <Box flex={1}>
+          <>
+            <Flex
+              justifyContent="center"
+              alignItems="center"
+              p={2}
+              mt={1}
+              mb={1}
+            >
               <Box>
-                <Text className={styles["job-title"]}>Software Engineer</Text>
+                <Image
+                  borderRadius="full"
+                  boxSize="100px"
+                  objectFit="cover"
+                  src="https://i.pinimg.com/736x/0a/b3/bf/0ab3bfaacfa2399a18576d8d8c151da8.jpg"
+                  alt="Dan Abramov"
+                  mr={2}
+                />
               </Box>
-              <Box>
-                <Flex justifyContent="flex-start" alignItems="center">
-                  <Text>PT. Rekso Nasional Food (RNF)</Text>
-                  <Icon viewBox="0 0 200 200" color="black" size={12}>
-                    <path
-                      fill="currentColor"
-                      d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
-                    />
-                  </Icon>
-                  <Text>Full time</Text>
-                </Flex>
+              <Box flex={1}>
+                <Box>
+                  <Text className={styles["job-title"]}>Software Engineer</Text>
+                </Box>
+                <Box>
+                  <Flex justifyContent="flex-start" alignItems="center">
+                    <Text>PT. Rekso Nasional Food (RNF)</Text>
+                    <Icon viewBox="0 0 200 200" color="black" size={12}>
+                      <path
+                        fill="currentColor"
+                        d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+                      />
+                    </Icon>
+                    <Text>Full time</Text>
+                  </Flex>
+                </Box>
+                <Box>
+                  <Flex justifyContent="flex-start" alignItems="center">
+                    <Text>Jan 2022 - Present</Text>
+                    <Icon viewBox="0 0 200 200" color="black" size={12}>
+                      <path
+                        fill="currentColor"
+                        d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+                      />
+                    </Icon>
+                    <Text>8 mos</Text>
+                  </Flex>
+                </Box>
+                <Box>
+                  <Text>Jakarta, Indonesia</Text>
+                </Box>
               </Box>
-              <Box>
-                <Flex justifyContent="flex-start" alignItems="center">
-                  <Text>Jan 2022 - Present</Text>
-                  <Icon viewBox="0 0 200 200" color="black" size={12}>
-                    <path
-                      fill="currentColor"
-                      d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
-                    />
-                  </Icon>
-                  <Text>8 mos</Text>
-                </Flex>
-              </Box>
-              <Box>
-                <Text>Jakarta, Indonesia</Text>
-              </Box>
-            </Box>
-          </Flex>
+            </Flex>
+            <Divider w="50%" />
+          </>
         );
       })}
     </Box>
