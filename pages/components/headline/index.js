8297cb777bef3bc4de0f9d3fbe107dee1409cb96
Brosnan Andhika <andhikabrosnan@gmail.com>
2022-11-20 20:31:49 +0700

    test

--- pages/components/headline/index.js
+++ pages/components/headline/index.js
@@ -4,7 +4,7 @@ import styles from "./style.module.css";
 
 const Headline = () => {
   const isMobile = isMobileHandler();
-  console.log("error pls test");
+  console.log("error pls test zz");
 
   return (
     <Box className={styles["container-headline"]}>
