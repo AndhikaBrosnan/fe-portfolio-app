71b83b582c8cb716d4549d8b140e0d33d2e2a98c
Brosnan Andhika <andhikabrosnan@gmail.com>
2022-11-20 18:59:19 +0700

    fix(structure): add image & fix structuring

--- pages/components/headline/index.js
+++ pages/components/headline/index.js
@@ -1,19 +1,35 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
-import { isMobileHandler } from "../../../helpers/responsive";
-import styles from "./style.module.css";
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { isMobileHandler } from '../../../helpers/responsive';
+import styles from './style.module.css';
 
 const Headline = () => {
   const isMobile = isMobileHandler();
-  return (
-    <Box className={styles["container-headline"]}>
-      <Heading as="h1" size="xl">
-        Arfandi Usemahu
-      </Heading>
-      <Heading as="h2" size={isMobile ? "sm" : "md"} mb={9}>
-        Software Developer
-      </Heading>
 
-      <Text as="i">"Kalo ada yang bagus, ya saya hina. Biar balance!!"</Text>
+  return (
+    <Box className={styles['container-headline']}>
+      <Flex justifyContent="flex-start" alignItems="center">
+        <Box className={styles['profile-photo']}>
+          <Image
+            src="https://bit.ly/dan-abramov"
+            alt="profilePhoto"
+            objectFit="cover"
+            borderRadius="full"
+            boxSize="150px"
+          />
+        </Box>
+
+        <Box>
+          <Heading as="h1" size="xl">
+            Arfandi Usemahu
+          </Heading>
+          <Heading as="h2" size={isMobile ? 'sm' : 'md'} mb={9}>
+            Software Developer
+          </Heading>
+          <Text as="i">
+            "Kalo ada yang bagus, ya saya hina. Biar balance!!"
+          </Text>
+        </Box>
+      </Flex>
     </Box>
   );
 };
